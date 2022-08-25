d10099f0a7247400fdf809db87caded91038d6f5
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2022-08-25 02:34:45 +0200

    Refreshing movie every day 2:00 After guessing movie now receiving chosen movie attributes and comparasion

--- src/main/java/moviedle/movie/moviedle/Classic.java
+++ src/main/java/moviedle/movie/moviedle/Classic.java
@@ -3,11 +3,15 @@ package moviedle.movie.moviedle;
 import moviedle.movie.Movie;
 import moviedle.movie.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Locale;
 
 @RestController
+@EnableScheduling
 @RequestMapping("games/classic")
 public class Classic {
 
@@ -19,26 +23,31 @@ public class Classic {
         this.movieService = movieService;
     }
 
-    @PutMapping(path = "/start")
+   @PutMapping(path = "/start")  //TEMP METHOD
     public void startGame() {
         movieToGuess = movieService.getRandomMovie();
         System.out.println(movieToGuess.getTitle());
     }
 
+    @Scheduled(cron = "0 0 2 * * ?")  //2:00: 0 0 2 - seconds,minute,hour
+    public void generateRandomMovieEveryDay(){
+        movieToGuess = movieService.getRandomMovie();
+        System.out.println(movieToGuess.getTitle());
+    }
 
     @GetMapping(path = "guess/{title}")
-    public ComparedAttributes guessMovie(@PathVariable("title") String title) {
+    public List guessMovie(@PathVariable("title") String title) {
         if (movieToGuess != null) {
             Movie chosenMovie = movieService.getMovieByTitle(title.toLowerCase(Locale.ROOT));
             if (!movieExistsInDB(chosenMovie)) {
                 throw new IllegalStateException("Movie does not exist");
             }else {
-                return MovieComparator.compareChosenMovie(chosenMovie,movieToGuess);
+                //todo return attributes titles
+                return List.of(chosenMovie,MovieComparator.compareChosenMovie(chosenMovie,movieToGuess));
             }
         }
         throw new IllegalStateException("You need to start game first!");
     }
-
     private boolean movieExistsInDB(Movie movie){
         return movie != null;
     }
